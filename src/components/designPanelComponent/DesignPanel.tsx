cbfb07a1e09c46ac0707d2c5fd4e6177c5d8ade3
Julio Perez <julioperez.dev@gmail.com>
2023-05-10 23:56:11 -0300

    change position o pagination panel

--- src/components/designPanelComponent/DesignPanel.tsx
+++ src/components/designPanelComponent/DesignPanel.tsx
@@ -51,20 +51,6 @@ export default function DesignPanel() {
     return (
         <div className={styles.designPanelBase}>
             <div className={styles.imagePanel}>
-                <div className={styles.imagesList}>
-                    {currentItems && currentItems.map(particularDesign => (
-                        <div className={styles.particularDesign} key={particularDesign}>
-                            <Image
-                                src={particularDesign}
-                                alt={particularDesign.substring(8)}
-                                width={180}
-                                height={180} />
-                            <div className={styles.buttonBase}>
-                                <p>👀</p> 
-                            </div>
-                        </div>
-                    ))}
-                </div>
                 <ReactPaginate
                     breakLabel="..."
                     nextLabel=">"
@@ -80,6 +66,35 @@ export default function DesignPanel() {
                     nextClassName={styles.nextButtons}
 
                 />
+                <div className={styles.imagesList}>
+                    {currentItems && currentItems.map(particularDesign => (
+                        <div className={styles.particularDesign} key={particularDesign}>
+                            <Image
+                                src={particularDesign}
+                                alt={particularDesign.substring(8)}
+                                width={180}
+                                height={180} />
+                            <div className={styles.buttonBase}>
+                                <p>👀</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                {/* <ReactPaginate
+                    breakLabel="..."
+                    nextLabel=">"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={3}
+                    pageCount={pageCount}
+                    previousLabel="<"
+                    renderOnZeroPageCount={null}
+                    containerClassName={styles.containerPagination}
+                    pageClassName={styles.pageClassName}
+                    breakClassName={styles.breakClassName}
+                    previousClassName={styles.previousButtons}
+                    nextClassName={styles.nextButtons}
+
+                /> */}
             </div>
             <div className={styles.line}></div>
             <div className={styles.preSelectedProductBase}>
@@ -108,20 +123,20 @@ export default function DesignPanel() {
                         </div>
                         <div className={styles.shirtSize}>
                             <Image
-                            src='/options/optionFront.png'
-                            alt='modelo de camiseta con diseño frontal'
-                            width={50}
-                            height={50}/>
+                                src='/options/optionFront.png'
+                                alt='modelo de camiseta con diseño frontal'
+                                width={50}
+                                height={50} />
                             <Image
-                            src='/options/optionBack.png'
-                            alt='modelo de camiseta con diseño reves'
-                            width={50}
-                            height={50}/>
+                                src='/options/optionBack.png'
+                                alt='modelo de camiseta con diseño reves'
+                                width={50}
+                                height={50} />
                             <Image
-                            src='/options/optionSmall.png'
-                            alt='modelo de camiseta con diseño en el pecho'
-                            width={50}
-                            height={50}/>
+                                src='/options/optionSmall.png'
+                                alt='modelo de camiseta con diseño en el pecho'
+                                width={50}
+                                height={50} />
                         </div>
                         <div className={styles.shirtSize}>
                             <p>-</p>
@@ -129,8 +144,87 @@ export default function DesignPanel() {
                             <p>+</p>
                         </div>
                         <div>
-                            <p className={styles.confirmButton}>AGREGAR</p>    
+                            <p className={styles.confirmButton}>AGREGAR</p>
                         </div>
+                    </div>
+
+                </div>
+                <div className={styles.cartBase}>
+                    <div className={styles.subTotalCartBase}>
+                        <div className={styles.particularCartBase}>
+                            <Image
+                                src='/designs/springFramework.png'
+                                alt='modelo de camiseta con diseño frontal'
+                                width={50}
+                                height={50} />
+                            <Image
+                                src='/options/optionFront.png'
+                                alt='modelo de camiseta con diseño frontal'
+                                width={40}
+                                height={40} />
+                            <div className={styles.descriptionParticularCart}>
+                                <p>Java Mate design</p>
+                                <p>color: azul  talla: XL</p>
+                            </div>
+                            <div className={styles.cartSize}>
+                                <p>-</p>
+                                <p>1</p>
+                                <p>+</p>
+                            </div>
+                            <p>30500 $</p>
+                        </div>
+                        <div className={styles.particularCartBase}>
+                            <Image
+                                src='/designs/subteSQL.png'
+                                alt='modelo de camiseta con diseño frontal'
+                                width={50}
+                                height={50} />
+                            <Image
+                                src='/options/optionFront.png'
+                                alt='modelo de camiseta con diseño frontal'
+                                width={40}
+                                height={40} />
+                            <div className={styles.descriptionParticularCart}>
+                                <p>Java Mate design</p>
+                                <p>color: rojo  talla: XL</p>
+                            </div>
+                            <div className={styles.cartSize}>
+                                <p>-</p>
+                                <p>1</p>
+                                <p>+</p>
+                            </div>
+                            <p>30500 $</p>
+                        </div>
+                        <div className={styles.particularCartBase}>
+                            <Image
+                                src='/designs/deployEveryday.png'
+                                alt='modelo de camiseta con diseño frontal'
+                                width={50}
+                                height={50} />
+                            <Image
+                                src='/options/optionBack.png'
+                                alt='modelo de camiseta con diseño frontal'
+                                width={40}
+                                height={40} />
+                            <div className={styles.descriptionParticularCart}>
+                                <p>Java Mate design</p>
+                                <p>color: negro  talla: S</p>
+                            </div>
+                            <div className={styles.cartSize}>
+                                <p>-</p>
+                                <p>1</p>
+                                <p>+</p>
+                            </div>
+                            <p>30500 $</p>
+                        </div>
+                    </div>
+                    <div className={styles.totalPriceBase}>
+                        <p className={styles.totalPriceDescription}>Total</p>
+                        <div className={styles.totalPriceData}>
+                        <p className={styles.totalPrice}>40500 $</p>
+                        <p className={styles.totalPriceCurrency}>pesos argentinos</p>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
